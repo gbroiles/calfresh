2fb68b59d0a5705ec6d081179c8ca3210d9d6214
Greg Broiles <gbroiles@gmail.com>
2023-08-14 17:57:05 -0700

    begin migration to multi-year lookup tables

---
+++ calfresh_tables.py
@@ -0,0 +1,17 @@
+lookup = {
+    "2023": {
+        "begin_date": (2022, 10, 1),
+        "end_date": (2023, 9, 30),
+        "gross_income_limit": [2266, 3052, 3840, 4626, 5412, 6200, 6986, 7772],
+        "net_income_limit": [1133, 1526, 1920, 2313, 2706, 3100, 3493, 3886],
+        "std_deduct": [193, 193, 193, 193, 225, 258, 258, 258],
+        "max_benefit": [281, 516, 740, 939, 1116, 1339, 1480, 1691],
+        "irt": [1473, 1984, 2496, 3007, 3518, 4030, 4541, 5052],
+        "gross_income_increment": 758,
+        "net_income_increment": 394,
+        "std_deduct_increment": 0,
+        "max_benefit_increment": 211,
+        "irt_increment": 512,
+        "lookup_max": 8,
+    }
+}
